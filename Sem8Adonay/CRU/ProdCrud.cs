0c119e8129efc8ec03ec9c92b620a6dd5e92e126
Adonay66 <Zeladaadonay7@gmail.com>
2023-03-24 20:28:34 -0600

    Cod9

---
+++ Sem8Adonay/CRU/ProdCrud.cs
@@ -0,0 +1,28 @@
+﻿using Sem8Adonay.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sem8Adonay.CRU
+{
+    internal class ProdCrud
+    {
+        public void AgregueProduct(Producto ParametroProducto)
+        {
+            using (AlmacenContext BaseDeDatos = new AlmacenContext())
+            {
+                Producto product = new Producto();
+                product.Nombre = ParametroProducto.Nombre;
+                product.Descripcion = ParametroProducto.Descripcion;
+                product.Precio = ParametroProducto.Precio;
+                product.Stock = ParametroProducto.Stock;
+                BaseDeDatos.Add(product);
+                BaseDeDatos.SaveChanges();
+
+            }
+        }
+
+    }
+}
